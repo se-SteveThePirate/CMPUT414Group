bb8e9e37355875aa65d14e34968cf775efed464c
esinglet <esinglet@ualberta.ca>
2015-04-16 17:31:59 -0600

    Removed old Files

--- C3D/414-C3D/Point.cpp
+++
@@ -1,7 +0,0 @@
-#include "Point.h"
-
-Point::Point(double xi, double yi, double zi){
-	x = xi;
-	y = yi;
-	z = zi;
-}
--- C3D/414-C3D/Point.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-class Point
-{
-	public: 
-		double x, y, z;
-		Point(double x, double y, double z);
-};
-
