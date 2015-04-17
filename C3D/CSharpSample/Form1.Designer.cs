1235022dc55a88bdf6471a9ebace383920e1c92c
tcline1 <tcline@ualberta.ca>
2015-04-16 21:54:33 -0600

    C# C3D sample just in case we want to integrate

---
+++ C3D/CSharpSample/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace CSharpSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(12, 12);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(93, 31);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "Open C3D File...";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "C3D files (*.c3d)|*.c3d|All files (*.*)|*.*";
+            this.openFileDialog1.RestoreDirectory = true;
+            this.openFileDialog1.SupportMultiDottedExtensions = true;
+            this.openFileDialog1.Title = "Open a C3D file";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 49);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(587, 284);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 392);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnOpenFile);
+            this.Name = "Form1";
+            this.Text = "C3D Server C# Sample";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ C3D/CSharpSample/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using C3DSERVERLib;
+namespace CSharpSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+                C3DSERVERLib.C3D m_c3d = new C3DSERVERLib.C3D();
+
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                OpenC3dFile(openFileDialog1.FileName);
+            }
+        }
+        private void AddLog(String s)
+        {
+            textBox1.Text = textBox1.Text + Environment.NewLine + s;
+        }
+        private void OpenC3dFile(String sFilePath)
+        {
+            textBox1.Text = "";
+            C3DSERVERLib.C3D m_c3d;
+            Boolean IsOpen = false;
+            m_c3d = new C3DSERVERLib.C3D();
+           
+            try
+            {
+                if (m_c3d.Open(sFilePath, 3) == 0)
+                {
+                    AddLog("File was opened");
+                    IsOpen = true;
+                    int nParamIndex = m_c3d.GetParameterIndex("TEST", "HELLO_WORLD");
+                    if (nParamIndex < 0)
+                    {
+                        AddLog("Could not find Parameter TEST:HELLO_WORLD");
+                        int nGroupIndex = m_c3d.GetGroupIndex("TEST");
+                        if (nGroupIndex < 0)
+                        {
+                            AddLog("Could not find Group TEST");
+                            nGroupIndex = m_c3d.AddGroup(0, "TEST", "Test Group by c3d server sample", 0);
+                            if (nGroupIndex >= 0)
+                            {
+                                AddLog("Group TEST was added");
+
+                            }
+                            else
+                            {
+                                AddLog("Group TEST could not be added");
+                            }
+                        }
+                        if (nGroupIndex >= 0)
+                        {
+                            int nGroupNumber = m_c3d.GetGroupNumber(nGroupIndex);
+                            Byte[] vDimensions = { 16, 3 };
+                            String[] vData = { "Data Item 1", "Data Item 2", "Data Item 3" };
+                            nParamIndex = m_c3d.AddParameter("HELLO_WORLD", "Hello World Test", "TEST", 0, -1, 2, vDimensions, vData);
+                            if (nParamIndex >= 0)
+                            {
+                                AddLog("Parameter TEST:HELLO_WORLD was created");
+                            }
+                            else
+                            {
+                                AddLog("Parameter could not be created");
+
+                            }
+                        }
+
+                    }
+                    else
+                    {
+                        int nDone;
+                        nDone = m_c3d.DeleteParameter(nParamIndex);
+                        if (nDone == 1)
+                        {
+                            AddLog("Parameter TEST:HELLO_WORLD was deleted");
+                            int nGroupIndex  = m_c3d.GetGroupIndex("TEST");
+                            if (nGroupIndex >= 0)
+                            {
+                                    nDone = m_c3d.DeleteGroup(nGroupIndex, 0);
+                                    if (nDone == 1)
+                                    {
+                                        AddLog("Group TEST has been deleted");
+                                    }
+                                    else
+                                    {
+                                        AddLog("Group TEST could not be deleted");
+
+                                    }
+                            }
+                        }
+                        else
+                        {
+                            AddLog("Parameter could not be deleted");
+
+                        }
+                    }
+
+                    int nSaved = m_c3d.SaveFile(sFilePath, m_c3d.GetFileType());
+                    if (nSaved == 1)
+                    {
+                        AddLog("File was saved");
+                    }
+                    else
+                    {
+                        AddLog("File could not be saved");
+                    }
+                    m_c3d.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Could not open the file");
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            finally
+            {
+                if (IsOpen)
+                {
+                    m_c3d.Close();
+                }
+            }
+        }
+    }
+}
